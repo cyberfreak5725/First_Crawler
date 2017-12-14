61c5258459c1b2ea933c2f4c88e6cac2743ec699
Anukul chand <anukulchand1@gmail.com>
2017-12-14 22:36:58 +0530

    Working web crawler

---
+++ general.py
@@ -0,0 +1,58 @@
+import os
+# Each website Crawled is different project
+#i am going to have a lot of shared functions in here
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('creating project '+directory)
+        os.makedirs(directory)
+
+
+# Creating data files(files that go into project directory)
+
+# multithreaded crawler(spes)
+
+#create queue and crawled files(if not created)
+
+def created_data_files(project_name,base_url):
+    queue= project_name+'/queue.txt'
+    crawled=project_name+'/crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue,base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled,'')
+
+
+#create a new file
+
+def write_file(path, data):
+    f=open(path, 'w')
+    f.write(data)
+    f.close()
+
+#created_data_files('spidercrawl','https://coolchand5725.com')
+
+#writing data to an existing file
+
+def append_to_file(path,data):
+    f=open(path,'a')
+    f.write(data+'\n')
+    f.close()
+
+#clearing the files(contents)
+
+def delete_file_content(path):
+    f=open(path,'w')
+    pass
+
+def file_to_set(file_name):
+    results=set()
+    with open(file_name,'rt') as f:
+        for line in f:
+            results.add(line.replace('\n',''))
+    return results
+
+def set_to_file(links,file):
+    delete_file_content(file)
+    for link in sorted(links):
+        append_to_file(file,link)
---
+++ main.py
@@ -0,0 +1,49 @@
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from general import *
+
+PROJECT_NAME= 'crawler' #in caps because in python constant are not there so it is convention to write var name in capital
+HOMEPAGE ='https://coolchand5725.com/'
+DOMAIN_NAME =get_domain_name(HOMEPAGE)
+QUEUE_FILE=PROJECT_NAME+'/queue.txt'
+CRAWLED_FILE=PROJECT_NAME+'/crawled.txt'
+NUMBER_OF_THREADS = 3
+queue=Queue()     #thread
+Spider(PROJECT_NAME,HOMEPAGE,DOMAIN_NAME)
+
+#Create worker threads gonna die when main exits
+def create_spiders():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+#DO the next job in the queue
+
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name,url)
+        queue.task_done()
+
+
+
+#each queued link is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+#check if there are itms in queue if so crawl them
+
+def crawl():
+    queued_links=file_to_set(QUEUE_FILE)
+    if len(queued_links)>0:
+        print(str(len(queued_links))+" links in the queue")
+        create_jobs()
+
+#create_spiders()
+#crawl()
